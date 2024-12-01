ca87a192bdca4c53b2a631e1db1b966b75807f55
alexbukatov <97853041+alexbukatov@users.noreply.github.com>
2024-12-01 19:09:16 +0300

    Корректировка блока с курсами валют

--- src/components/HomePageComponents/CoursesBlock/CoursesBlock.tsx
+++ src/components/HomePageComponents/CoursesBlock/CoursesBlock.tsx
@@ -19,31 +19,25 @@ const RATE_ITEMS: TrateItems[] = [
 const CoursesBlock = () => {
   return (
     <section className={styles.currency}>
-      <div className={styles.currency__top}>
-        <h3>Exchange rate in internet bank</h3>
-        <p className={styles.currency__topUpdate}>Update every 15 minutes, MSC 09.08.2022</p>
-      </div>
+      <h3>Exchange rate in internet bank</h3>
+      <p className={styles.currency__update}>Update every 15 minutes, MSC 09.08.2022</p>
       <div className={styles.currency__rate}>
-        <div>
-          <p className={styles.currency__rateHeader}>Currency</p>
-          <div className={styles.currency__rateMiddle}>
-            <ul className={styles.currency__rateList}>
-              {RATE_ITEMS.map((currencyObj, index) => (
-                <li key={currencyObj.name} className={styles.currency__rateItem}>
-                  <p className={styles.currency__rateItem__name}>{currencyObj.name}:</p>
-                  <p className={styles.currency__rateItem__text} tabIndex={index + 1}>
-                    {currencyObj.price}
-                  </p>
-                </li>
-              ))}
-            </ul>
-            <img src={currencySvg} alt="Currency illustration" aria-label="Currency illustration" />
-          </div>
+        <p className={styles.currency__rateHeader}>Currency</p>
+        <div className={styles.currency__rateMiddle}>
+          <ul className={styles.currency__rateList}>
+            {RATE_ITEMS.map((currencyObj, index: number) => (
+              <li key={currencyObj.name} className={styles.currency__rateItem}>
+                <p className={styles.currency__rateItem__name}>{currencyObj.name}:</p>
+                <p tabIndex={index + 1}>{currencyObj.price}</p>
+              </li>
+            ))}
+          </ul>
+          <img src={currencySvg} alt="Currency illustration" aria-label="Currency illustration" />
         </div>
-        <Link tabIndex={8} to="/" className={styles.currency__rateBottom}>
-          All courses
-        </Link>
       </div>
+      <Link tabIndex={8} to="/" className={styles.currency__rateLink}>
+        All courses
+      </Link>
     </section>
   );
 };
