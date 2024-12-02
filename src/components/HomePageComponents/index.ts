aa6211bb41c4b122ccafa733497063fbaec66aee
alexbukatov <97853041+alexbukatov@users.noreply.github.com>
2024-12-02 18:47:59 +0300

    правка

--- src/components/HomePageComponents/index.ts
+++ src/components/HomePageComponents/index.ts
@@ -2,5 +2,4 @@ export {default as AdvantagesBlock}  from "./AdvantagesBlock/AdvantagesBlock";
 export {default as CoursesBlock}  from "./CoursesBlock/CoursesBlock";
 export {default as FormSupport}  from "./FormSupport/FormSupport";
 export {default as MapBlock}  from "./MapBlock/MapBlock";
-export {default as MenuBurger}  from "./MenuBurger/MenuBurger";
 export {default as OptionDesignCard}  from "./OptionDesignCard/OptionDesignCard";
